87bfbab515dca0a0faf38cc263b412bb6e0e1777
Jeanne Mercier <jeanne.mercier.2602@gmail.com>
2024-12-12 17:15:22 +0100

    rm todo

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,6 @@ mod parser;
 use crate::equation_solver::solver;
 use crate::parser::parser;
 
-/*      TODO
-
-    - tests unitaires ?
-    - tests programme
-    - print l'équation réduite avant de check le degree
-
-*/
-
 fn main() -> Result<(), String> {
     match parser() {
         Ok(equation_terms) => solver(equation_terms),
